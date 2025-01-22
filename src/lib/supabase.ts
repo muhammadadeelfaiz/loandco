2cfb10ca7953516614fc13b4f3b2140063d20b69
lastman1o1 <120045958+lastman1o1@users.noreply.github.com>
2025-01-22 17:28:59 +0400

    added superbase details

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_PROJECT_URL.supabase.co';
-const supabaseKey = 'YOUR_ANON_KEY';
+const supabaseUrl = 'https://svdgniviotecguehvtig.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2ZGduaXZpb3RlY2d1ZWh2dGlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NTE5ODAsImV4cCI6MjA1MzEyNzk4MH0.X6V8bw0SX-ZppIeIL6tXhqiXTej2shzJJ2PNxICWe8g';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
